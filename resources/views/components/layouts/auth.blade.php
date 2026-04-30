921df6fc76201ea798c0bf035bd24de1642f63a6
lebefriedlich <noval.akbar.906@gmail.com>
2026-04-30 13:57:36 +0700

    feat: create authentication layout component for application pages

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -10,18 +10,11 @@
 
     <link rel="shortcut icon" href="{{ asset('logo.svg') }}">
     <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css">
-    <script src="{{ asset('assets/js/config.js') }}"></script>
-
     @livewireStyles
 </head>
 
 <body>
     {{ $slot }}
-
-    <script src="{{ asset('assets/libs/preline/preline.js') }}"></script>
-    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/js/app.js') }}"></script>
 
     @livewireScripts
 </body>
